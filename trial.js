2116cbc08db722f12a3d0a7c41282c13585fe918
Maureen <88616706+Njeri254@users.noreply.github.com>
2022-11-01 10:12:53 +0300

    Add files via upload

---
+++ trial.js
@@ -0,0 +1,18 @@
+//menu hamburger start
+$(".hamb").click(function(){
+    $(".hamb").toggleClass("change");
+      $("nav").slideToggle(200);
+  });    
+//menu hamburger ends
+//menu 
+$(document).ready(function() {
+$('li a.menu').click(
+    function(e) {
+    e.preventDefault();
+    var dropDown = $(this).parent().find("ul.a_menu");                   
+    dropDown.slideToggle(200);
+    $('li a.menu').parent().find("ul.a_menu").not(dropDown).slideUp(200);
+     
+         });    
+    });
+    
